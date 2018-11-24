4e5292f3330cb7632da523c144692a09a5d60a31
早津渉 <hayatsuwataru@hayatsuwatarunoMacBook-puro.local>
2018-11-24 09:40:46 +0900

    dotenvを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require('path');
+const path = require("path");
 const GasPlugin = require("gas-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
+  mode: "development",
+  entry: "./src/index.ts",
+  node: {
+    fs: "empty"
+  },
   devtool: false,
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader'
+        use: "ts-loader"
       }
     ]
   },
   resolve: {
-    extensions: [
-      '.ts'
-    ]
+    extensions: [".ts"]
   },
-  plugins: [
-    new GasPlugin()
-  ]
+  plugins: [new GasPlugin(), new Dotenv()]
 };
