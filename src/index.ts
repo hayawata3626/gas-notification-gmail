fed8c256066d2dc36657e403952c6dc9e4d9ba5c
早津渉 <hayatsuwataru@hayatsuwatarunoMacBook-puro.local>
2018-11-24 09:43:22 +0900

    slackに通知する処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-import { SheetService } from './sheet.service';
-
 declare var global: any;
 
-global.createNewFile = (): void => {
-  const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
+global.notifificationSlackGmail = (): void => {
+  const postUrl = process.env.SLACK_URL;
+  const username = 'hoge';
+  const icon = ':ramen:';
+  const channel = '#gas_test';
+  var date = new Date();
+  const query = 'is:unread from:notifications@m.teachable.com';
+  var myThreads = GmailApp.search(query, 0, 60); //条件にマッチしたスレッドを取得
+  var myMsgs = GmailApp.getMessagesForThreads(myThreads); //スレッドからメールを取得する　→二次元配列で格納
+  for (var i = 0; i < myMsgs.length; i++) {
+    var targetMessage = myMsgs[i][0].getPlainBody();
+    myThreads[i].markRead();
+    var jsonData = {
+      username: username,
+      channel: channel,
+      icon_emoji: icon,
+      text: targetMessage
+    };
+    var payload: string = JSON.stringify(jsonData);
+    var options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+      method: 'post',
+      contentType: 'application/json',
+      payload: payload
+    };
+    UrlFetchApp.fetch(postUrl, options);
+  }
 };
